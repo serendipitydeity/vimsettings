e78c714883ab3262dc523113df48fa0933c1b069
Jae Ho Jeon <deity@snu.ac.kr>
2014-04-20 00:12:24 +0900

    added module adder script

---
+++ module_adder.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+is_url='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
+
+if [ "$#" -ne 1 ] || [[ !($1 =~ $is_url) ]] ; then
+  echo "Usage: $0 https://github.com/example/path"
+else
+  URL=$1
+  BUNDLE_NAME=${URL##*/}
+
+  git submodule add $URL .vim/bundle/$BUNDLE_NAME
+fi
