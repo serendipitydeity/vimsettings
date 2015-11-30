149a489635f844f588de178a755652da88d7d145
Sukwon Choi <swchoi06@wafflestudio.com>
2015-11-30 09:40:52 +0900

    added cd command on shell script to make it available to use setup script on different directory

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #/bin/bash
 
 echo "This script replaces the current vim settings!"
+CURRENT_DIR=$PWD
+BASE_DIR=$(dirname $0)
+
+cd $BASE_DIR
 git submodule init
 git submodule update
 rm -rf ~/.vimrc
 rm -rf ~/.vim/
 cp -R .vim ~/
 cp -R .vimrc ~/
+
+cd $CURRENT_DIR
