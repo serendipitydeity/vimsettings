a16ad79237332ea4e64ac1e7c276a6d257619a42
jay.jh <jay.jh@kakaocorp.com>
2016-10-19 17:33:45 +0900

    added `dirname $0` to support ansible

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "This script replaces the current vim settings!"
 
 rm -rf ~/.vimrc
 rm -rf ~/.vim/
-cp .vimrc ~/
+cp `dirname $0`/.vimrc ~/
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
